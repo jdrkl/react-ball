a491bdd88111e80ed11bba8543bcbd9590bffacb
jdkrl <164361926+jdrkl@users.noreply.github.com>
2025-03-10 15:30:34 +0000

    Basic player and field code implemented

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-ball
 Football tactics tool allowing you to create, edit and, visualise football formations.
+
+### To-do
+
+- [ ] Implement field & players
+- [ ] Add opposition team
+- [ ] Add ability to name players
+- [ ] Add function to list out players on the field
+- [ ] Add player position indicator
+- [ ] Add Formations (Using CSS grid?)
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import Draggable from 'react-draggable'
 
 import './App.css'
 
+export function Player() {
+  const nodeRef = useRef(null);
 
+  return (
+    <Draggable nodeRef={nodeRef}>
+      <div className='player'
+        ref={nodeRef}
+      ></div>
+    </Draggable>
+  );
+}
+
+export default function Field() {
+  return (
+    <div className='field' draggable="false">
+      <div className='centre-circle'>
+        <div className='centre-dot'></div>
+      </div>
+
+      <div className='goal-left'></div>
+      <div className='goal-right'></div>
+
+      <div className='penalty-box-left'></div>
+      <div className='penalty-box-right'></div>
+      
+      <div className='half-line'></div>
+
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+    </div>
+  );
+}
